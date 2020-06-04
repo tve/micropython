6b8887752d2c9b67f524f81462853cbb8e870de6
Thorsten von Eicken <tve@voneicken.com>
2020-06-03 22:42:37 -0700

    esp32: make machine.soft_reset() work in main.py

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -78,6 +78,7 @@ void mp_task(void *pvParameter) {
     mp_thread_init(pxTaskGetStackStart(NULL), MP_TASK_STACK_LEN);
     #endif
     uart_init();
+    machine_init();
 
     // TODO: CONFIG_SPIRAM_SUPPORT is for 3.3 compatibility, remove after move to 4.0.
     #if CONFIG_ESP32_SPIRAM_SUPPORT || CONFIG_SPIRAM_SUPPORT
@@ -123,7 +124,10 @@ soft_reset:
     pyexec_frozen_module("_boot.py");
     pyexec_file_if_exists("boot.py");
     if (pyexec_mode_kind == PYEXEC_MODE_FRIENDLY_REPL) {
-        pyexec_file_if_exists("main.py");
+        int ret = pyexec_file_if_exists("main.py");
+        if (ret & PYEXEC_FORCED_EXIT) {
+            goto soft_reset_exit;
+        }
     }
 
     for (;;) {
@@ -139,6 +143,8 @@ soft_reset:
             }
         }
     }
+
+soft_reset_exit:
 
     #if MICROPY_BLUETOOTH_NIMBLE
     mp_bluetooth_deinit();
@@ -156,6 +162,7 @@ soft_reset:
 
     // deinitialise peripherals
     machine_pins_deinit();
+    machine_deinit();
     usocket_events_deinit();
 
     mp_deinit();
