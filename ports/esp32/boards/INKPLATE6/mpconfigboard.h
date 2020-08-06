55eed4f5c9b8102d35887e6621876da364b238a4
Thorsten von Eicken <tve@voneicken.com>
2020-08-05 17:21:33 -0700

    add support for INKPLATE 6 with reduced SPIRAM

---
+++ ports/esp32/boards/INKPLATE6/mpconfigboard.h
@@ -0,0 +1,2 @@
+#define MICROPY_HW_BOARD_NAME "Inkplate 6"
+#define MICROPY_HW_MCU_NAME "ESP32"
---
+++ ports/esp32/boards/INKPLATE6/mpconfigboard.mk
@@ -0,0 +1,5 @@
+SDKCONFIG += boards/sdkconfig.base
+SDKCONFIG += boards/sdkconfig.spiram
+SDKCONFIG += boards/INKPLATE6/sdkconfig.board
+
+PART_SRC = partitions-ota.csv
