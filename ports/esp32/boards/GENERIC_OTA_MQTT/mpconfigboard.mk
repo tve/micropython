bc26a6ec962eb691ba74577368934e14b992614e
Thorsten von Eicken <tve@voneicken.com>
2020-12-26 12:52:23 -0800

    esp32: fix TvE voard definitions

--- ports/esp32/boards/GENERIC_OTA_MQTT/mpconfigboard.mk
+++ ports/esp32/boards/GENERIC_OTA_MQTT/mpconfigboard.mk
@@ -1,6 +1,6 @@
 SDKCONFIG += boards/sdkconfig.base
 SDKCONFIG += boards/sdkconfig.240mhz
-SDKCONFIG += boards/GENERIC_OTA/sdkconfig.board
+SDKCONFIG += boards/GENERIC_OTA_MQTT/sdkconfig.board
 
 PART_SRC = partitions-ota.csv
 
